e4211cc11b06d08570827d60fd8f87bdcea79178
Maximilian Zauner <zaunermax@gmail.com>
2019-01-25 22:41:53 +0100

    refactor(style): extracted global style

--- src/App.js
+++ src/App.js
@@ -1,23 +1,6 @@
 import React from 'react'
-import { createGlobalStyle } from 'styled-components/macro'
 import { StdCraComponent } from './components/craComponent'
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
-      "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
-      sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-  }
-  
-  code {
-    font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
-      monospace;
-  }
-`
+import { GlobalStyle } from './components/app/globalStyle'
 
 export const App = () => (
   <>
